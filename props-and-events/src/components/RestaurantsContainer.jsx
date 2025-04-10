a311454a60be3c2a8d22ed586c3f32b9149021a7
Ben Mojo <155706745+oakes777@users.noreply.github.com>
2025-04-09 21:25:38 -0700

    JS commit used container component to render multiple presentational components

--- props-and-events/src/components/RestaurantsContainer.jsx
+++ props-and-events/src/components/RestaurantsContainer.jsx
@@ -1,6 +1,14 @@
+import { restaurants } from "../data/data.js";
+
+import Restaurant from "./Restaurant.jsx";
+
+//Higher-Order Components
 function RestaurantsContainer(props) {
   return (
     <div className="restaurantContainer">
+      {restaurants.map((restaurant) => (
+        <Restaurant restaurant={restaurant} key={restaurant.id} />
+      ))}
     </div>
   );
 }
